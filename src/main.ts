5b3c82ae8a73ef629203127f83ee5dc91e55c5ad
Ivantsov Ilya <iwww68@gmail.com>
2020-04-26 10:47:35 +0300

    Set Global prefix and port

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.setGlobalPrefix('queue');
   app.useGlobalPipes(new ValidationPipe({ skipMissingProperties: true }));
-  await app.listen(3000);
+  await app.listen(3333);
 }
 bootstrap();
